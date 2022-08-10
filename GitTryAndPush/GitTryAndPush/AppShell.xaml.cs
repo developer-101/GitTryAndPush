6039b4e2159768a6b84d18976bae5c34e07404be
Rustam <Rustam@HP-RUSTAM>
2022-08-10 11:11:42 +0500

    Add project files.

---
+++ GitTryAndPush/GitTryAndPush/AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using GitTryAndPush.ViewModels;
+using GitTryAndPush.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace GitTryAndPush
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
---
+++ GitTryAndPush/GitTryAndPush/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using GitTryAndPush.Models;
+using GitTryAndPush.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GitTryAndPush.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
